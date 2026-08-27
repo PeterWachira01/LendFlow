f215c715788eba06584eeb712f99ac741e8e5e78
PeterWachira01 <peterwachira994@gmail.com>
2026-08-27 05:29:14 +0300

    Fix Render database connection

--- server/src/db.ts
+++ server/src/db.ts
@@ -7,12 +7,19 @@ pg.types.setTypeParser(1082, (value) => value);
 
 const { Pool } = pg;
 
-const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: Number(process.env.DB_PORT),
-});
+const pool = new Pool(
+  process.env.DATABASE_URL
+    ? {
+        connectionString:
+          process.env.DATABASE_URL,
+      }
+    : {
+        user: process.env.DB_USER,
+        host: process.env.DB_HOST,
+        database: process.env.DB_NAME,
+        password: process.env.DB_PASSWORD,
+        port: Number(process.env.DB_PORT),
+      },
+);
 
 export default pool;
